0358124d6d5720d22f362f80c9eeaa2413e1b859
Francesco Meli <francesco.meli@intecom.it>
2016-02-17 03:57:07 -0800

    changed company details from 2 to one column

--- resources/views/companies/company.blade.php
+++ resources/views/companies/company.blade.php
@@ -1,32 +1,42 @@
 <div class="table-responsive">
 	<table class="table table-striped table-condensed table-hover">
 		<tr>
-			<th>Company</th>
-			<td> {{ $company->name }} </td>
+			<th class="col-xs-6 col-md-3 col-lg-2">Company</th>
+			<td> {{ $company->name }} </td>			
+		</tr>
+		<tr>
 			<th>Country</th>
 			<td>{{ $company->country }}</td>
 		</tr>
 		<tr>
 			<th>State</th>
 			<td>{{ $company->state }}</td>
+		</tr>
+		<tr>
 			<th>City</th>
 			<td>{{ $company->city }}</td>
 		</tr>
 		<tr>	
 			<th>Address</th>
 			<td>{{ $company->address }}</td>
+		</tr>
+		<tr>
 			<th>Zip Code</th>
 			<td>{{ $company->zip_code }}</td>
 		</tr>
 		<tr>
 			<th>Support Type</th>
 			<td>{{ isset($company->support_type_id) ? $company->support_type->name : '' }}</td>
+		</tr>
+		<tr>
 			<th>Account Manager</th>
 			<td> @if (isset($company->account_manager->account_manager_id)) <a href="{{ route('people.show', $company->account_manager->company_person->person->id) }}"> {{ $company->account_manager->company_person->person->name()  }} @endif </a> </td>
 		</tr>
 		<tr>
 			<th>Connection Option</th>
 			<td> {!! isset($company->connection_type_id) ? "<b>" . $company->connection_type->name . "</b>: ". $company->connection_type->description : '' !!} </td>
+		</tr>
+		<tr>
 			<th>Group Email</th>
 			<td>{{ $company->group_email }}</td>
 		</tr>
