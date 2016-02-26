d00942b9181ef524211e35a72855b0fd9fb44e4b
Francesco Meli <francesco.meli@intecom.it>
2016-02-25 23:16:40 -0600

    grouped image gallery to scroll images per item and not per ticket

--- resources/views/files/attachments.blade.php
+++ resources/views/files/attachments.blade.php
@@ -4,7 +4,7 @@
 			<div class="col-xs-3 col-ms-2 col-sm-2 col-md-1 col-lg-1">
 				<div class="thumbnail">
 					<div data-toggle="tooltip" data-placement="right" title="{{ $attachment->name() }}">
-						<a href="{{ $attachment->path() }}" @if ($attachment->is_image()) data-gallery @endif>
+						<a href="{{ $attachment->path() }}" @if ($attachment->is_image()) data-gallery="#{{$attachment->resource->id}}" @endif>
 							<img src="{{ $attachment->thumbnail() }}" alt="...">
 						</a>
 					</div>
