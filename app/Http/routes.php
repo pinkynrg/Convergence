68d37ada7dcc686aa0e66ccc608e0b69c9d123de
Francesco Meli <francesco.meli@intecom.it>
2016-03-16 12:00:19 -0500

    Made route /file/{id} show page accessible by anyone

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,9 +14,9 @@
 Route::get('login', array('uses' => 'LoginController@showLogin', 'as' => 'login.index'));
 Route::post('login', array('uses' => 'LoginController@doLogin', 'as' => 'login.login'));
 Route::get('logout', array('uses' => 'LoginController@doLogout', 'as' => 'login.logout'));
-
 Route::get('start',['uses' => 'LoginController@start', 'as' => 'login.start']);
 Route::post('start/{id}',['uses' => 'LoginController@storeInfo', 'as' => 'login.store_info']);
+Route::get('files/{id}',['uses' => 'FilesController@show', 'as' => 'files.show']);
 
 Route::group(array('middleware' => 'auth'), function() {
 
@@ -194,7 +194,6 @@ Route::group(array('middleware' => 'auth'), function() {
 	Route::match(['get'], 'statistics', ['uses' => 'StatisticsController@index', 'as' => 'statistics.index']);
 
 	//attachments
-	Route::get('files/{id}',['uses' => 'FilesController@show', 'as' => 'files.show']);
 	Route::post('files', ['uses' => 'FilesController@upload', 'as' => 'files.upload']);
 	Route::delete('files/{id}', ['uses' => 'FilesController@destroy', 'as' => 'files.destroy']);
 
