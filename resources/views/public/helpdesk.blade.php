e498bbdcaddcf169f5f4d30a2ef956488270fb08
Francesco Meli <francesco.meli@intecom.it>
2016-04-11 19:38:49 -0500

    Added style for iframe video helpdesk page

--- resources/views/public/helpdesk.blade.php
+++ resources/views/public/helpdesk.blade.php
@@ -12,8 +12,10 @@
 	If you do not have an account please contact <b>847-329-7717</b> and select to speak to our Help Desk who will give you information about getting an account and for general support questions.
 </p>
 
-<center>
-	<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://player.vimeo.com/video/40067453?title=0&amp;byline=0&amp;portrait=0&amp;color=ff9933' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-</center>
+<div class='embed-container'>
+	<iframe src='http://player.vimeo.com/video/40067453?title=0&amp;byline=0&amp;portrait=0&amp;color=ff9933' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen>
+		
+	</iframe>
+</div>
 
 @endsection
