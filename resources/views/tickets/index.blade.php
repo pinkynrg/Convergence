5b5b84e7d7a4af9ab185d75bdb4da3a184440426
Francesco Meli <francesco.meli@intecom.it>
2016-06-01 22:25:13 -0700

    Added division and status filters for customers

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -2,34 +2,35 @@
 
 @section('content')
 
-	@if (Auth::user()->active_contact->isE80())
 
 		<div class="visible-xs" id="expand_filters">
 			<h4>Expand Filters</h4>
 		</div>
 
 		<div id="filters" class="row hidden-xs">
-			
-			<div class="col-xs-12 col-ms-4 col-md-2">
-				{!! 
-					Form::BSMultiSelect("tickets.company_id", $companies, 
-					["title" => "companies", "selected_text" => "Companies", "class" => "multifilter", "search" => "true", "data-size" => "5", "value" => "id", "label" => "!name"]);
-				!!}
-			</div>
 
-			<div class="col-xs-12 col-ms-4 col-md-2">
-				{!! 
-					Form::BSMultiSelect("tickets.assignee_id", $employees, 
-					["title" => "assignees", "selected_text" => "Assignees", "class" => "multifilter", "search" => "true", "data-size" => "5", "value" => "id", "label" => ["!last_name"," ","!first_name"]]);
-				!!}
-			</div>
+			@if (Auth::user()->active_contact->isE80())
+				<div class="col-xs-12 col-ms-4 col-md-2">
+					{!! 
+						Form::BSMultiSelect("tickets.company_id", $companies, 
+						["title" => "companies", "selected_text" => "Companies", "class" => "multifilter", "search" => "true", "data-size" => "5", "value" => "id", "label" => "!name"]);
+					!!}
+				</div>
 
-			<div class="col-xs-12 col-ms-4 col-md-2">
-				{!! 
-					Form::BSMultiSelect("tickets.creator_id", $employees, 
-					["title" => "creators", "selected_text" => "Creators", "class" => "multifilter", "search" => "true", "data-size" => "5", "value" => "id", "label" => ["!last_name"," ","!first_name"]]) 
-				!!}
-			</div>
+				<div class="col-xs-12 col-ms-4 col-md-2">
+					{!! 
+						Form::BSMultiSelect("tickets.assignee_id", $employees, 
+						["title" => "assignees", "selected_text" => "Assignees", "class" => "multifilter", "search" => "true", "data-size" => "5", "value" => "id", "label" => ["!last_name"," ","!first_name"]]);
+					!!}
+				</div>
+
+				<div class="col-xs-12 col-ms-4 col-md-2">
+					{!! 
+						Form::BSMultiSelect("tickets.creator_id", $employees, 
+						["title" => "creators", "selected_text" => "Creators", "class" => "multifilter", "search" => "true", "data-size" => "5", "value" => "id", "label" => ["!last_name"," ","!first_name"]]) 
+					!!}
+				</div>
+			@endif
 
 			<div class="col-xs-12 col-ms-4 col-md-2">
 				{!! 
@@ -45,13 +46,18 @@
 				!!}
 			</div>
 
+			@if (!Auth::user()->active_contact->isE80())
+				<div class="col-xs-12 col-ms-4 col-md-2"></div>
+				<div class="col-xs-12 col-ms-4 col-md-2"></div>
+				<div class="col-xs-12 col-ms-4 col-md-2"></div>
+			@endif
+
 			<div class="col-xs-12 col-ms-4 col-md-2" id="reset_filters">
 				<button type="button" class="btn btn-default">Reset Filters</button>
 			</div>
 
 		</div>
 
-	@endif
 
 	@include('tickets/tickets', array('tickets' => $tickets))
 	
